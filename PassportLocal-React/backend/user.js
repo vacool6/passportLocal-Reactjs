1fb87918b42400c6cb34d3c229939b8ddf5151cb
vacool6 <42382545+vacool6@users.noreply.github.com>
2022-01-17 21:57:48 +0530

    Update user.js

--- PassportLocal-React/backend/user.js
+++ PassportLocal-React/backend/user.js
@@ -5,3 +5,16 @@ const userSchema = mongoose.Schema({
 })
 
 module.exports = mongoose.model('User', userSchema)
+
+//----------Approach B 
+
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({});
+
+userSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model('User', userSchema);
+
+//-------if choose this approach then npm i passport-local-mongoose must be installed in additional
