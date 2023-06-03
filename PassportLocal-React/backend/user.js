a7ad5576db7d6679f809d374d4506a5a494cc2e9
Kittur Sameer <42382545+vacool6@users.noreply.github.com>
2023-06-03 20:00:36 +0530

    fix/logout not working

--- PassportLocal-React/backend/user.js
+++ PassportLocal-React/backend/user.js
@@ -1,20 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
-    username: String,
-    password: String
-})
+  username: String,
+  password: String,
+});
 
-module.exports = mongoose.model('User', userSchema)
-
-//----------Approach B 
-
-//const mongoose = require('mongoose');
-//const passportLocalMongoose = require('passport-local-mongoose');
-//const Schema = mongoose.Schema;
-
-//const userSchema = new Schema({});
-
-//userSchema.plugin(passportLocalMongoose);
-//module.exports = mongoose.model('User', userSchema);
-
-//-------if choose this approach then npm i passport-local-mongoose must be installed in additional
+module.exports = mongoose.model("User", userSchema);
